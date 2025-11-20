7489cc9841d72b58e10dcf2f900ccc2c0dad4a0a
Muhammad Atif <atifm1190@gmail.com>
2025-11-20 10:14:34 +0500

    changes reverted

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,10 +19,6 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: "SA Consulting", 
   description: "SA Consulting Ltd is a accounting and consulting firm that provides audit, tax, and consulting services to public and private entities.",
-  icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon.ico",
-  },
 };
 
 export default function RootLayout({
